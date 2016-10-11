449d1f5363ef05850e6e3c3404f7a6df5e039d2f
luizfelipecirqueira <lfcs18ts@gmail.com>
2016-10-10 21:17:47 -0300

    Conexao do Banco de CursoDAO

--- SistemaAcademico1/SistemaAcademico1/Models/Curso/CursoDAO.cs
+++ SistemaAcademico1/SistemaAcademico1/Models/Curso/CursoDAO.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using SistemaAcademico1.Models.Curso;
+using System.Configuration;
 
 namespace SistemaAcademico1.Models.Curso
 {
     public class CursoDAO
     {
+         private String conexao;
+
+        public CursoDAO () {
+            conexao = ConfigurationManager.ConnectionStrings["StringConexao"].ConnectionString;
+        }
     }
 }
